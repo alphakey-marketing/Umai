5062f5698e4459946cfa7e77ccefd274da07a5a8
alphakey-marketing <curryfishcake@gmail.com>
2026-03-13 08:05:29 +0000

    don't change vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   server: {
     host: '0.0.0.0',
-    port: 5173,
-    allowedHosts: 'all',
-  },
-  optimizeDeps: {
-    exclude: ['@xenova/transformers'],
-  },
-  worker: {
-    format: 'es',
-  },
-});
+  }
+})
